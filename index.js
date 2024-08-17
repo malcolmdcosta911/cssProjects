a9a5104e77fde05ae70f58f7c66a9a4cc69390f5
malcolmdcosta911 <malcolmdcosta911@gmail.com>
2024-08-17 23:13:06 +0530

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const allItems = document.getElementsByClassName("item");
 
 Array.from(allItems).forEach((element) => {
-  //   element.classList.remove("active");
   element.addEventListener("click", (e) => {
-    // console.log("yes");
-    //remove prev  from all prev
-    Array.from(allItems).forEach((element) =>
-      element.classList.remove("active")
-    );
-    //add class to current
+    removeActiveClasses();
+    //add active class to current
     element.classList.add("active");
   });
 });
+
+function removeActiveClasses() {
+  //remove active from all
+  Array.from(allItems).forEach((element) => element.classList.remove("active"));
+}
