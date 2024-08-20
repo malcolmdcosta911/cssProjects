86d8d7bbebeb43ed30372a0466b5e008b0e886c4
malcolmdcosta911 <malcolmdcosta911@gmail.com>
2024-08-21 00:53:25 +0530

    third commit

--- index.js
+++ index.js
@@ -1,58 +1,19 @@
-const allItems = document.querySelectorAll(".step");
-const prevBtn = document.getElementById("prev");
-const nextBtn = document.getElementById("next");
+const allItems = document.getElementsByClassName("step");
 
 let step = 1;
 
-//default
-if (step == 1) {
-  prevBtn.disabled = true;
-}
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
 
 nextBtn.addEventListener("click", (e) => {
-  if (step < allItems.length) {
-    step++;
-  }
-  
-
-  onStepChnage();
+  if (step == 4) return;
+  Array.from(allItems)[step].classList.add("active");
+  step++;
+  // console.log("step", step);
 });
 
 prevBtn.addEventListener("click", (e) => {
-  if (step > 1) {
-    step--;
-  }
-  onStepChnage();
+  if (step == 1) return;
+  step--;
+  Array.from(allItems)[step].classList.remove("active");
 });
-
-const onStepChnage = () => {
-
-
-  //1/3-->33 2/3-->66 3/3-->1
-  const width = ((step - 1) / (allItems.length - 1)) * 100;
-  // console.log("width", width);
-  progress.style.width = width + "%";
-
-  //handle disable
-  if (step == 1) {
-    prevBtn.disabled = true;
-    //nextBtn do nothing
-  } else if (step == 4) {
-    nextBtn.disabled = true;
-    //prevBtn do nothing
-  } else {
-    prevBtn.disabled = false;
-    nextBtn.disabled = false;
-  }
-
-  // console.log("step", step);
-
-  //inc/dec step
-  allItems.forEach((item, index) => {
-    if (index + 1 <= step) {
-      item.classList.add("active");
-    } else {
-      item.classList.remove("active");
-    }
-  });
-};
