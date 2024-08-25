96066d278528caee935137a5ad34fc842f03ea19
malcolmdcosta911 <malcolmdcosta911@gmail.com>
2024-08-25 23:54:57 +0530

    first push

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-// const showBtn = document.getElementById("show");
-// const hideBtn = document.getElementById("hide");
-// const container = document.getElementById("container");
-// const menuCircle = document.getElementById("menuCircle");
-// const navbar = document.getElementById("navbar");
+const fill = document.querySelector(".fill");
+const boxes = document.querySelectorAll(".empty-box");
 
+boxes.forEach((box) => {
+  box.addEventListener("dragover", (event) => {
+    // prevent default to allow drop
+    event.preventDefault();
+  });
 
+  box.addEventListener("drop", (e) => {
+    e.preventDefault();
+    box.classList.remove("hover");
+    //When the appendChild() method is called, it appends a node to the list of children of a specified parent node.
+    // If the given node already exists in the document, appendChild() removes it from its current position and places it at the new position.
+    box.append(fill);
+  });
+  box.addEventListener("dragenter", (event) => {
+    box.classList.add("hover");
+  });
+  box.addEventListener("dragleave", (event) => {
+    box.classList.remove("hover");
+  });
+});
 
-// showBtn.addEventListener("click", (e) => {
-//   container.classList.add("show");
-//   menuCircle.classList.add("rotate");
-//   navbar.classList.add("active");
-// });
+fill.addEventListener("dragstart", (e) => {
+  fill.classList.add("drag");
+  //setTimeout to add delay and allow classes "fill" and "drag still be present"
+  setTimeout(() => {
+    fill.classList = "";
+  }, 0);
+});
 
-// hideBtn.addEventListener("click", (e) => {
-//   container.classList.remove("show");
-//   menuCircle.classList.remove("rotate");
-//   navbar.classList.remove("active");
-
-// });
+fill.addEventListener("dragend", (e) => {
+  fill.classList.add("fill");
+});
